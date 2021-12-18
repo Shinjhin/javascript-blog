5146b33e62c570cb588b104d0dd1fa21bc449fc5
Shinjhin <huberthubka199@gmail.com>
2021-12-18 04:44:34 +0100

    end of function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -35,8 +35,48 @@ function titleClickHandler(event){
     targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    console.log(titleList);
+    
+  /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+    console.log('articles: ', articles)
+    
+    let html = '';
+    
+     for(let article of articles){  
+         
+         /* get the article id */
+        const articleId = article.getAttribute('id'); 
+         
+         /* find the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML; 
+         
+         /* get the title from the title element */
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+         
+        console.log('linkHTML', linkHTML);
+
+   /* insert link into html variable */
+    html = html + linkHTML;
+     }
+  titleList.innerHTML = html;
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+    for(let link of links){
+        link.addEventListener('click', titleClickHandler);
+    }
 }
+
+generateTitleLinks();
+
+ 
